00c9bf3c11909e18636f7316faca4a471285ea29
Ben Richey <b.richeypg@gmail.com>
2022-02-25 15:20:49 -0500

    Implement training on wine data
    Implemented the training functions to train a multiple linear regression
    model for the wine data. This involves writing a function that performs
    gradient descent and a function that computes the value of the partial
    derivative of the loss function with respect to the given theta that's
    being updated. The training stops after a set number of time steps.
    
    CS460G HW2

--- CS460G/HW2/regressinator.py
+++ CS460G/HW2/regressinator.py
@@ -33,7 +33,9 @@ import sys
 import os
 import csv
 import ast
-import math
+import numpy as np
+from tqdm import tqdm
+import yaml
 
 # Debugging
 import ipdb
@@ -41,7 +43,14 @@ import ipdb
 WINE_FILE = "winequality-red.csv"
 SYNTHETIC_FILES = ["synthetic-1.csv", "synthetic-2.csv"]
 
-def load_file_data(data_dir, synthetic, filename):
+REG_MODELS = {} # Contains theta values for each model
+ALPHAS = {}     # Contains alpha parameter for each model
+ALPHAS["wine"] = 0.00000001
+ALPHAS["synthetic-1.csv"] = 0.00000001
+ALPHAS["synthetic-2.csv"] = 0.00000001
+TIME_STEPS = 10000
+
+def load_file_data(data_dir, filename):
     data = {}
     for root, dirs, files in os.walk(data_dir):
         for f in files: # Iterate through files in data_dir
@@ -50,30 +59,83 @@ def load_file_data(data_dir, synthetic, filename):
             try:
                 with open(os.path.join(root, f)) as csvfile:
                     reader = csv.reader(csvfile)
-                    if not synthetic:
-                        for row in reader:
-                            for i in range(len(row)):
-                                data[row[i]] = []
-                            break
-                        keys = [key for key in data.keys()]
-                        for row in reader:
-                            for i in range(len(row)):
-                                data[keys[i]].append(ast.literal_eval(row[i]))
-                    else:
-                        keys_established = False
-                        for row in reader:
-                            if not keys_established:
-                                for i in range(len(row)):
-                                    data["f{}".format(i)] = []
-                                keys = [k for k in data.keys()]
-                                keys_established = True
-                            else:
-                                for i in range(len(row)):
-                                    data[keys[i]].append(ast.literal_eval(row[i]))
+                    for row in reader:
+                        for i in range(len(row)):
+                            data[row[i]] = []
+                        break
+                    keys = [key for key in data.keys()]
+                    for row in reader:
+                        for i in range(len(row)):
+                            data[keys[i]].append(ast.literal_eval(row[i]))
             except IOError:
                 print("Error: Unable to open file '{}'".format(f))
                 sys.exit()
     return data
+
+def preprocess_data(data):
+    """
+        Converts each feature value for each feature to a value between zero and 1
+        while preserving the weights of each value.
+        @param data: dictionary containing data set. Keys = feature names,
+                     Values = feature vals for each example. One special key, called
+                     'classification', has a value of a list of the correct classification
+                     for each example.
+    """
+    for feature in data.keys():
+        if feature == "classification":
+            continue
+        the_sum = sum(data[feature])
+        new_vals = [val / the_sum for val in data[feature]]
+        data[feature] = new_vals
+    return
+
+def linear_reg(x_vals, model):
+    """
+        Linear Regression Function
+        @param x_vals: numpy ndarray of input values for each feature where first element is always 1
+                       to account for theta_0. The rest are the theta_n to be multiplied by x_n
+        @param model: which model to use
+        @return:  h(X) = theta_0 + summation[theta_i*x_i for i in range(num_features)]
+    """
+    return np.dot(x_vals, REG_MODELS[model].transpose())
+
+def lin_loss_pdwrtj(j, model, data):
+    """
+        Partial derivative w/ respect to feature j of the loss function (0.5*mean squared error)
+        for linear regression
+        @param j: the index of the theta we are computing the partial derivative w.r.t
+        @param model: the model we're using
+        @param data: dictionary containing data set. Keys = feature names,
+                     Values = feature vals for each example. One special key, called
+                     'classification', has a value of a list of the correct classification
+                     for each example.
+        @return: value computed from partial derivative of loss fnxn w.r.t theta_j
+    """
+    sum = 0
+    keys = [ k for k in data.keys() if k != "classification"]
+    m = len(data[keys[0]])
+    for i in range(m):
+        vals = [data[f][i] for f in keys]
+        vals.insert(0, 1.0) # Prepend w/ 1 to account for theta_0
+        x_vals = np.asarray(vals, dtype=float)
+        sum += (linear_reg(x_vals, model) - data["classification"][i]) * x_vals[j]
+    return sum * (1 / m)
+
+def linear_gradient_descent(data, model):
+    """
+        Trains theta_0 through theta_n values for n feature linear gradient descent
+        @param data: dictionary where key = feature name and value = list of values
+                     for that feature. One special key, called 'classification',
+                     has a value of a list of the correct classification for each
+                     example.
+        @param model: model to train
+        @result: Trains the given model TIME_STEPS times, storing the theta values
+                 in REG_MODELS[model]. Uses full batch update for updating theta vals.
+    """
+    for step in tqdm(range(TIME_STEPS), desc="Training {} model".format(model)):
+        for j in range(len(REG_MODELS[model])):
+            new_theta = REG_MODELS[model][j] - (ALPHAS[model] * lin_loss_pdwrtj(j, model, data))
+            REG_MODELS[model][j] = new_theta
 
 if __name__ == "__main__":
 
@@ -82,8 +144,17 @@ if __name__ == "__main__":
     except IndexError as err:
         print("Usage: python3 regressinator.py <path to data directory>")
         sys.exit()
-    
-    wine_data = load_file_data(data_dir, False, WINE_FILE)
-    synthetic_data = {}
-    for i in range(len(SYNTHETIC_FILES)):
-        synthetic_data[SYNTHETIC_FILES[i]] = load_file_data(data_dir, True, SYNTHETIC_FILES[i])
+
+    wine_data = load_file_data(data_dir, WINE_FILE)
+    wine_data["classification"] = wine_data["quality"]
+    del wine_data["quality"]
+    preprocess_data(wine_data)
+    # Randomly initialize theta vals
+    REG_MODELS["wine"] =  np.asarray([np.random.rand() for theta in range(len(wine_data.keys()))])
+    linear_gradient_descent(wine_data, "wine")
+    try:
+        with open(os.path.join(data_dir, "model_parameters.yaml")) as f:
+            yaml.dump(REG_MODELS, f)
+            print("Saved training parameters to {}".format(os.path.join(data_dir, "model_parameters.yaml")))
+    except IOError as err:
+        print("Error: unable to store training data to {}".format(os.path.join(data_dir, "model_parameters.yaml")))
