8c402128a818fa4e77bb777a0e0657275e99edaf
Ben Richey <b.richeypg@gmail.com>
2022-03-03 17:06:00 -0500

    Improve data structures
    Optimized training time by improving the data structures used to store
    training data such that during training, no new data structures need to
    be created. Instead of extracting and creating a list from the feature
    values of a given example in the data dictionary during each partial
    derivative calculation, go ahead and create a numpy matrix before
    starting training in the proper format.
    
    Added method to take in data loaded from disk into a dictionary, and
    return a matrix where each row is the feature values for one example and
    each column is the set of feature values for one feature. This method
    also deletes all elements from the dictionary except for the
    classification data.
    
    CS460G HW2

--- CS460G/HW2/regressinator.py
+++ CS460G/HW2/regressinator.py
@@ -129,6 +129,29 @@ def preprocess_data(data):
         data[feature] = new_vals
     return
 
+def convert_data_to_array(data):
+    """
+        Converts dataset into NxM matrix where N = number of examples and M = number of features.
+        Each row is the feature values for one example. Each column is the feature values for one
+        feature.
+        @param data: dictionary containing single feature data set. Keys = feature names,
+                     Values = feature vals for each example. One special key, called
+                     'classification', has a value of a list of the correct classification
+                     for each example.
+        @return: ndarray as described above.
+        @side effect: deletes all elements in the dictionary except the classification list
+    """
+    keys = [k for k in data.keys() if k != "classification"]
+    arrays = []
+    for i in range(len(data[keys[0]])):
+        vals = [data[f][i] for f in keys]
+        vals.insert(0, 1.0) # Prepend w/ 1 to account for theta_0
+        arrays.append(vals)
+    for f in keys:
+        del data[f]
+
+    return np.asarray(arrays, dtype=float)
+
 def basis_expansion(data, order):
     """
         Perform basis expansion on single feature data by creating new features using
@@ -149,9 +172,9 @@ def basis_expansion(data, order):
         new_data["order_{}".format(i)] = [data["order_1"][j]**i for j in range(len(data["order_1"]))]
     return new_data
 
-def linear_reg(x_vals, model):
+def regression(x_vals, model):
     """
-        Linear Regression Function
+        Multivariate Regression Function
         @param x_vals: numpy ndarray of input values for each feature where first element is always 1
                        to account for theta_0. The rest are the theta_n to be multiplied by x_n
         @param model: which model to use
@@ -159,35 +182,31 @@ def linear_reg(x_vals, model):
     """
     return np.dot(x_vals, REG_MODELS[model].transpose())
 
-def loss_pdwrtj(j, model, data):
+def loss_pdwrtj(j, model, classifications, data):
     """
         Partial derivative w/ respect to feature j of the loss function (0.5*mean squared error)
         for multiple regression
         @param j: the index of the theta we are computing the partial derivative w.r.t
         @param model: the model we're using
-        @param data: dictionary containing data set. Keys = feature names,
-                     Values = feature vals for each example. One special key, called
-                     'classification', has a value of a list of the correct classification
-                     for each example.
+        @param classifications: dictionary with only one element 'classification' whose
+                                value is a list of the classifications for each example
+        @param data: ndarray object where each row is the feature values for each feature for one
+                     example. The columns are the set of feature values for one feature.
         @return: value computed from partial derivative of loss fnxn w.r.t theta_j
     """
     sum = 0
-    keys = [ k for k in data.keys() if k != "classification"]
-    m = len(data[keys[0]])
+    m = len(data[:, 0])
     for i in range(m):
-        vals = [data[f][i] for f in keys]
-        vals.insert(0, 1.0) # Prepend w/ 1 to account for theta_0
-        x_vals = np.asarray(vals, dtype=float)
-        sum += (linear_reg(x_vals, model) - data["classification"][i]) * x_vals[j]
+        sum += (regression(data[i], model) - classifications["classification"][i]) * data[i, j]
     return sum * (1 / m)
 
-def gradient_descent(data, model):
+def gradient_descent(classifications, data, model):
     """
         Trains theta_0 through theta_n values for n feature gradient descent
-        @param data: dictionary where key = feature name and value = list of values
-                     for that feature. One special key, called 'classification',
-                     has a value of a list of the correct classification for each
-                     example.
+        @param classifications: dictionary with only one element 'classification' whose
+                                value is a list of the classifications for each example
+        @param data: ndarray object where each row is the feature values for each feature for one
+                     example. The columns are the set of feature values for one feature.
         @param model: model to train
         @result: Trains the given model TIME_STEPS times, storing the theta values
                  in REG_MODELS[model]. Uses full batch update for updating theta vals.
@@ -198,7 +217,7 @@ def gradient_descent(data, model):
         wheel.next()
         curr_loss = 0
         for j in range(len(REG_MODELS[model])):
-            loss = loss_pdwrtj(j, model, data)
+            loss = loss_pdwrtj(j, model, classifications, data)
             new_theta = REG_MODELS[model][j] - (ALPHAS[model] * loss)
             REG_MODELS[model][j] = new_theta
             curr_loss += loss
@@ -212,7 +231,7 @@ def gradient_descent(data, model):
             prev_loss = curr_loss
         else:
             prev_loss = curr_loss
-        m = mse(data, model)
+        m = mse(classifications, data, model)
         if m < BENCHMARKS[model]:
             print("\n\nMSE reached {} for {} model. Ending training.".format(m, model))
             print("{} model's theta 0 through theta n values are: {}".format(model, REG_MODELS[model].tolist()))
@@ -220,40 +239,38 @@ def gradient_descent(data, model):
     print("{} model unable to reach benchmark in {} time steps. It may have diverged or".format(model, TIME_STEPS)
           + " the hyper parameters need to be adjusted.")
 
-def mse(data, model):
+def mse(classifications, data, model):
     """
         Computes the mean squared error for the given model
-        @param data: dictionary where key = feature name and value = list of values
-                     for that feature. One special key, called 'classification',
-                     has a value of a list of the correct classification for each
-                     example.
+        @param metadata: dictionary where key = feature name and value = column index for that
+                         set of feature values in the data matrix.
+        @param data: ndarray object where each row is the feature values for each feature for one
+                     example. The columns are the set of feature values for one feature.
         @param model: model to train
         @return: mean squared error of model on training data
     """
     sum = 0
-    keys = [ k for k in data.keys() if k != "classification"]
-    m = len(data[keys[0]])
+    m = len(data[:, 0])
     for i in range(m):
-        vals = [data[f][i] for f in keys]
-        vals.insert(0, 1.0) # Prepend w/ 1 to account for theta_0
-        x_vals = np.asarray(vals, dtype=float)
-        sum += (linear_reg(x_vals, model) - data["classification"][i]) ** 2
+        sum += (regression(data[i], model) - classifications["classification"][i]) ** 2
     return sum * (1 / m)
 
 def train_multiple_order_poly_models(data, model):
     """
         Trains polynomial regression models for orders 2, 3, and 5 for the given dataset.
-        @param data: dictionary where key = feature name and value = list of values
-                     for that feature. One special key, called 'classification',
-                     has a value of a list of the correct classification for each
-                     example.
+        @param data: dictionary containing data set. Keys = feature names,
+                     Values = feature vals for each example. One special key, called
+                     'classification', has a value of a list of the correct classification
+                     for each example.
+        @param model: model to train
     """
     orders = [2, 3, 5]
     for order in orders:
         order_data = basis_expansion(data, order)
+        matrix_data = convert_data_to_array(order_data)
         curr_model = model + "_o{}".format(order)
         REG_MODELS[curr_model] = np.asarray([np.random.rand() for theta in range(order+1)])
-        gradient_descent(order_data, curr_model)
+        gradient_descent(order_data, matrix_data, curr_model)
 
 if __name__ == "__main__":
 
@@ -270,10 +287,12 @@ if __name__ == "__main__":
     del wine_data["quality"]
     # Standardize wine data
     preprocess_data(wine_data)
+    # Convert wine data to ndarray
+    matrix_wine_data = convert_data_to_array(wine_data)
     # Randomly initialize theta vals
-    REG_MODELS["wine"] =  np.asarray([np.random.rand() for theta in range(len(wine_data.keys()))])
+    REG_MODELS["wine"] =  np.asarray([np.random.rand() for theta in matrix_wine_data[0]])
     # Train wine model
-    gradient_descent(wine_data, "wine")
+    gradient_descent(wine_data, matrix_wine_data, "wine")
     # Load in synthetic 1 data
     synthetic_1_data = load_file_data(data_dir, SYNTHETIC_FILES[0], True)
     # Standardize synthetic 1 data
