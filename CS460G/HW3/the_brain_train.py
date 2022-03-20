07c79e3a22ec402ac0d3b360b14bc828ea7817f0
Ben Richey <b.richeypg@gmail.com>
2022-03-20 18:47:31 -0400

    Rough draft of training / testing FFNN for MNIST
    Initial rough draft of training and testing the feed forward neural
    network used to predict the numbers 0 and 1 from the MNIST data set. The
    FFNN features a 2 node hidden layer and a 1 node output layer.
    
    CS460G HW3

--- CS460G/HW3/the_brain_train.py
+++ CS460G/HW3/the_brain_train.py
@@ -32,13 +32,18 @@ import csv
 import ast
 import math
 import time
-from progress.spinner import Spinner
+import numpy as np
 
 import ipdb
 
 DATA_FILES = ["mnist_test_0_1.csv", "mnist_train_0_1.csv"]
+NUM_HIDDEN_NODES = 2
+NUM_FEATURES = 784
+ALPHA = 0.005
+TIME_STEPS = 100
+BENCHMARK = 95.0
 
-def load_data(data_dir):
+def load_file_data(data_dir, filename):
     """
         Load in training and test data for all models
         @param data_dir: directory containing training and test data
@@ -51,26 +56,40 @@ def load_data(data_dir):
     data = {}
     for root, dirs, files in os.walk(data_dir):
         for f in files: # Iterate through files in data_dir
-            if f not in DATA_FILES:
+            if f != filename:
                 continue
             try:
-                wheel = Spinner("Loading in {} data: ".format(f))
+                print("Loading in {} data...".format(f))
                 with open(os.path.join(root, f)) as csvfile:
                     reader = csv.reader(csvfile)
-                    data[f] = {}
-                    data[f]["examples"] = []
-                    data[f]["class"] = []
+                    data["examples"] = []
+                    data["class"] = []
                     i = 0
                     for row in reader:
-                        data[f]["examples"].append([ast.literal_eval(feature) for feature in row])
-                        data[f]["class"].append(data[f]["examples"][i].pop(0))
+                        data["examples"].append([float(ast.literal_eval(feature)) for feature in row])
+                        data["class"].append(data["examples"][i].pop(0))
                         i += 1
-                        wheel.next()
-                print("\n")
             except IOError:
                 print("Error: Unable to open file '{}'".format(f))
                 sys.exit()
     return data
+
+def preprocess_data(data):
+    """
+        Converts each feature value for each feature to a value between zero and 1
+        while preserving the relationships between values in a feature set.
+        @param data: list of lists where each inner list is the set of feature values
+                     for an example
+    """
+    for i in range(NUM_FEATURES):
+        the_max = data[:, i].max()
+        the_min = data[:, i].min()
+        if the_max == 0.0: # To account for features where all values are zero
+            new_vals = [0.0 for val in data[:, i]]
+        else:
+            new_vals = [((val - the_min) / (the_max - the_min)) for val in data[:, i]]
+        data[:, i] = new_vals
+    return np.asarray(data)
 
 def sigmoid(x):
     """
@@ -88,6 +107,71 @@ def sig_prime(x):
     """
     return sigmoid(x) * (1 - sigmoid(x))
 
+def train_model(examples, classes, hidden_weights, output_weights):
+    """
+        Trains feed forward neural network containing one hidden layer and a
+        single node output layer
+        @param examples: Nested numpy array of training data where rows are feature values
+        for one example. Columns are feature values for one feature.
+        @param classes: Single numpy array of class values for each example
+        @param hidden_weights: Nested numpy array where rows are weights for one node of
+        hidden layer. Columns are weights for one feature for all hidden layer nodes.
+        @param output_weights: Single numpy array where each element is the weight between
+        the a hidden layer node and the output layer node.
+        @return: tuple containing trained (hidden_weights, output_weights) pair
+    """
+    for epoch in range(TIME_STEPS):
+        for i in range(len(examples)): # This is one epoch
+            # Generate output at hidden layer
+            hidden_input = np.dot(hidden_weights, examples[i]) # NUM_HIDDEN_NODES x 1
+            hidden_output = np.asarray(list(map(sigmoid, hidden_input))) # NUM_HIDDEN_NODES x 1
+            # Generate input to output layer
+            output_layer_input = np.dot(output_weights.transpose(), hidden_output) # scalar value
+            output = sigmoid(output_layer_input)
+            # Calculate output error
+            error = classes[i] - output
+            # Compute output layer delta
+            output_delta = error * sig_prime(output_layer_input)
+            # Propagate delta to hidden layer
+            hidden_deltas = [sig_prime(hidden_input[j]) * output_weights[j] * output_delta
+                             for j in range(len(hidden_input))]
+            # Update weights for all layers
+            output_weights = [output_weights[j] + (ALPHA * hidden_output[j] * output_delta)
+                              for j in range(len(output_weights))]
+            output_weights = np.asarray(output_weights) # Convert back to numpy array
+            old_hidden_weights = hidden_weights
+            hidden_weights = []
+            for node_idx in range(len(old_hidden_weights)):
+                hidden_weights.append([old_hidden_weights[node_idx, j] +
+                                                       (ALPHA * examples[i, j] * hidden_deltas[node_idx])
+                                                       for j in range(NUM_FEATURES)])
+            hidden_weights = np.asarray(hidden_weights) # Convert back to numpy array
+            if i % 1000 == 0:
+                accuracy = compute_accuracy(examples, classes, hidden_weights, output_weights)
+                print("Accuracy: {}%".format(round(accuracy, 2)))
+                if accuracy > BENCHMARK:
+                    print("Training accuracy reached {}%. Ending Training.".format(round(accuracy, 2)))
+                    return (hidden_weights, output_weights)
+    print("Completed {} epochs and was unable to reach benchmark. Stopping training.".format(TIME_STEPS))
+    return None
+
+def compute_accuracy(examples, classes, hidden_weights, output_weights):
+    num_correct = 0
+    for i in range(len(examples)):
+        # Generate output at hidden layer
+        hidden_input = np.dot(hidden_weights, examples[i]) # NUM_HIDDEN_NODES x 1
+        hidden_output = np.asarray(list(map(sigmoid, hidden_input))) # NUM_HIDDEN_NODES x 1
+        # Generate input to output layer
+        output_layer_input = np.dot(output_weights.transpose(), hidden_output) # scalar value
+        output = sigmoid(output_layer_input)
+        if output > 0.5:
+            prediction = 1
+        else:
+            prediction = 0
+        if prediction == classes[i]:
+            num_correct += 1
+    return (num_correct / len(examples)) * 100
+
 if __name__ == "__main__":
     try:
         data_dir = sys.argv[1]
@@ -95,6 +179,41 @@ if __name__ == "__main__":
         print("Usage: python3 the_brain_train.py <path to directory containing training/test data>")
         sys.exit()
     start = time.time()
-    data = load_data(data_dir)
+    load_start = time.time()
+    training_data = load_file_data(data_dir, DATA_FILES[1])
+    load_end = time.time()
+    load_time = load_end - load_start
+    print("Beginning training")
+    train_start = time.time()
+    # Extract training data into ndarray objects
+    training_examples = training_data["examples"]
+    training_examples = np.asarray(training_examples)
+    training_class = training_data["class"]
+    # Preprocess feature data
+    training_examples = preprocess_data(training_examples)
+    # Randomly initialize layer weights
+    # NUM_HIDDEN_NODES x NUM_FEATURES
+    hidden_weights = np.asarray(np.random.uniform(-1.0, 1.0, (NUM_HIDDEN_NODES, NUM_FEATURES)))
+    # NUM_HIDDEN_NODES x 1 b/c we only want one output node
+    output_weights = np.asarray(np.random.uniform(-1.0, 1.0, NUM_HIDDEN_NODES))
+    # Begin training
+    weights = train_model(training_examples, training_class, hidden_weights, output_weights)
+    train_end = time.time()
+    # Load in test data
+    load_start = time.time()
+    test_data = load_file_data(data_dir, DATA_FILES[0])
+    load_end = time.time()
+    load_time += load_end - load_start
+    test_examples = np.asarray(test_data["examples"])
+    test_class = test_data["class"]
+    # Preprocess feature data
+    test_examples = preprocess_data(test_examples)
+    # Test model
+    hidden_weights = weights[0]
+    output_weights = weights[1]
+    accuracy = compute_accuracy(test_examples, test_class, hidden_weights, output_weights)
+    print("Test accuracy: {}%".format(round(accuracy, 2)))
     end = time.time()
     print("The program took {}s to run.".format(round(end - start, 2)))
+    print("The program spent {}s loading in file data".format(round(load_time, 2)))
+    print("The program spent {}s training".format(round(train_end - train_start, 2)))
